e72c7a7ce783f37ea249e2a96eac27af4b6b35a6
Davor Kapsa <davor.kapsa@gmail.com>
2021-08-14 19:27:30 +0200

    nvim: lua solarized theme

--- macos/.config/nvim/lua/dk/plugins.lua
+++ macos/.config/nvim/lua/dk/plugins.lua
@@ -2,6 +2,6 @@ local use = require('packer').use
 
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' 
-  use 'altercation/vim-colors-solarized'
+  use 'ishan9299/nvim-solarized-lua'
   use 'neovim/nvim-lspconfig'
 end)
