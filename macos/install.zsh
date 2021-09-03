27b49ec9d31efbbbe27cbb7c556c39e4eed8fe4b
Davor Kapsa <davor.kapsa@gmail.com>
2021-09-03 18:32:35 +0200

    macos: add nvim to install

--- macos/install.zsh
+++ macos/install.zsh
@@ -4,3 +4,7 @@ ln -sfv ~/dotfiles/macos/.zshenv ~/.zshenv
 
 mkdir -p ~/.config/git
 ln -sfv ~/dotfiles/macos/.config/git/config ~/.config/git/config
+
+mkdir -p ~/.config/nvim/lua
+ln -sfv ~/dotfiles/macos/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sfv ~/dotfiles/macos/.config/nvim/lua/dk ~/.config/nvim/lua
