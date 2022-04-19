2b4efe195219b14f8e980fa4763aead15a44e49c
Davor Kapsa <davor.kapsa@gmail.com>
2022-04-19 12:08:38 +0200

    nvim: fix lsp

--- macos/.config/nvim/lua/dk/lsp.lua
+++ macos/.config/nvim/lua/dk/lsp.lua
@@ -11,18 +11,18 @@ local on_attach = function(client, bufnr)
   local opts = { buffer=bufnr}
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '<leader>[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', '<leader>]', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  set('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  set('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  set('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  set('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  set('n', '<leader>[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  set('n', '<leader>]', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  set('n', '<leader>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 end
 
 nvim_lsp.gopls.setup {
